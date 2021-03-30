ee413f152986b05ddac781a130be3b2574c113bb
gao01java <60835579+gao01java@users.noreply.github.com>
2021-03-30 19:30:37 +0900

    3/30 6078 까지 시간:오후 7:30 분

---
+++ venv/Python1-100/6073.py
@@ -0,0 +1,7 @@
+a=int(input())
+
+a=(a-1)
+
+while a!=-1:
+    print(a)
+    a=a-1
---
+++ venv/Python1-100/6075.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=a
+
+
+# a=5 b=5
+# 当a大于等于0时 print文 一直输出 b-a 并且 a也是在自减
+while a>=0:
+    print(b-a)
+    a-=1
