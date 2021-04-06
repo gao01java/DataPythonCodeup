7937e91ec35baf44f3d281ac1e2b89028c84e6a3
gao01java <60835579+gao01java@users.noreply.github.com>
2021-04-06 13:52:58 +0900

    6087

--- venv/Python1-100/6082.py
+++ venv/Python1-100/6082.py
@@ -1,8 +1,8 @@
-a=int(input())
+    a=int(input())
 
-for i in range(1,a+1):
+    for i in range(1,a+1):
 
-    if i%10==3 or i%10==6 or i%10==9:
-        print("X",end=' ')
-    else:
-        print(i,end=' ')
+        if i%10==3 or i%10==6 or i%10==9:
+            print("X",end=' ')
+        else:
+            print(i,end=' ')
